f3595ed6d9c6b20da0cee13492c217f7392cb267
Kayla Jones <kaylalyne424@gmail.com>
2014-07-13 15:23:39 -0500

    finished

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 
 var prompt = require('sync-prompt').prompt;
 
-var mil = parseFloat(prompt('How far are you driving? ( in miles )'));
-var pr = parseFloat(prompt('How much is gas per gallon? ( 00.00 )'));
+var mileage = prompt('How far are you driving? ( in miles )');
+var price = prompt('How much is gas per gallon? ( 00.00 )');
 var type = prompt('What type of car do you drive? ( car/truck )');
-var mpg = parseFloat(prompt('What is your vehicles MPG rating? ( 25 )'));
-var tank = parseFloat(prompt('How big is your gas tanks? ( 16 ) '));
-var spd = parseFloat(prompt('How fast do you usually drive? ( 70 )'));
+var mpg = prompt('What is your vehicles MPG rating? ( 25 )');
+var tank = prompt('How big is your gas tanks? ( 16 ) ');
+var speed = prompt('How fast do you usually drive? ( 70 )');
 
+mileage = parseFloat(mileage);
+price = parseFloat(price);
+mpg = parseFloat(mpg);
+tank = parseFloat(tank);
+speed = parseFloat(speed);
 
 
 
 //calculate mpg
 //
 
-if (spd > 55){
-  var dif = spd - 55;
+if (speed > 55){
+  var dif = speed - 55;
 
   if ( type === 'car' ) {
-    mpg = mpg - dif;
+    mpg -= dif;
   } else {
-    mpg = mpg - (3 * dif);
+    mpg -= (3 * dif);
   }
-  
+
   if (mpg < 10) {
     mpg = 10;
   }
 }
 
 
-//number of stops
-//
-var gal = mil / mpg;
-var stops = Math.ceil(gal / tank);
+var gallons = mileage / mpg;
+var stops = Math.ceil(gallons / tank);
+var cost = gallons * price;
 
-//cost of trip
-//
-var cost = mil / mpg * pr;
-
-console.log('You are driving ' + mil + ' miles.');
-console.log('You will have to stop ' + stops + ' times and it will cost you ' + cost + ' dollars.');
+console.log('You are driving ' + mileage + ' miles.');
+console.log('You will have to stop ' + stops + ' times and it will cost you ' + cost.toFixed(2) + ' dollars.');
 
 
 
